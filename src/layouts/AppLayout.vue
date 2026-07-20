afba0e74898a4969447d731f6aee30f0cfbaf218
Yuri Aguiar <yuriaguiar@SGBR.intra>
2026-07-20 16:00:20 -0300

    Projeto iniciado

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppHeader from '@/components/layout/AppHeader.vue'
+import AppSidebar from '@/components/layout/AppSidebar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-[#f0f2f6]">
+    <AppHeader />
+    <AppSidebar />
+
+    <main class="min-h-screen pt-[42px] pl-[147px]">
+      <div class="p-6">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
